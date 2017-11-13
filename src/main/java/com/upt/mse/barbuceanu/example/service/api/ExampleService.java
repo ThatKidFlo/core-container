9c654561a31009f32d6aa3e41cfc0f6d9de416b5
Florin Barbuceanu <florin.barbuceanu@sap.com>
2017-11-13 23:47:42 +0200

    Adds an example service dependency.

---
+++ src/main/java/com/upt/mse/barbuceanu/example/service/api/ExampleService.java
@@ -0,0 +1,11 @@
+package com.upt.mse.barbuceanu.example.service.api;
+
+import java.util.List;
+
+/**
+ * @author Florin-Gabriel Barbuceanu, florin.barbuceanu@sap.com
+ * @since 13/11/2017
+ */
+public interface ExampleService {
+    List<Integer> getSomeNumbers();
+}
