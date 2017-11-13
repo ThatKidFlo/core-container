91d0cf8f72689c0f80608da2fc72209e31f218d0
Florin Barbuceanu <florin.barbuceanu@sap.com>
2017-11-13 23:57:32 +0200

    Refactors example service layer.

--- src/main/java/com/upt/mse/barbuceanu/example/service/api/EvenNumbersService.java
+++ src/main/java/com/upt/mse/barbuceanu/example/service/api/EvenNumbersService.java
@@ -6,6 +6,6 @@ import java.util.List;
  * @author Florin-Gabriel Barbuceanu, florin.barbuceanu@sap.com
  * @since 13/11/2017
  */
-public interface ExampleService {
-    List<Integer> getSomeNumbers();
+public interface EvenNumbersService {
+    List<Integer> getSomeEvenNumbers();
 }
---
+++ src/main/java/com/upt/mse/barbuceanu/example/service/impl/DefaultEvenNumberService.java
@@ -0,0 +1,30 @@
+package com.upt.mse.barbuceanu.example.service.impl;
+
+import com.upt.mse.barbuceanu.container.annotations.Component;
+import com.upt.mse.barbuceanu.example.repository.impl.InMemoryNumbersRepository;
+import com.upt.mse.barbuceanu.example.service.api.EvenNumbersService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author Florin-Gabriel Barbuceanu, florin.barbuceanu@sap.com
+ * @since 13/11/2017
+ */
+@Component
+public class DefaultEvenNumberService implements EvenNumbersService {
+
+    private final InMemoryNumbersRepository numbersRepository;
+
+    public DefaultEvenNumberService(InMemoryNumbersRepository numbersRepository) {
+        this.numbersRepository = numbersRepository;
+    }
+
+    @Override
+    public List<Integer> getSomeEvenNumbers() {
+        return numbersRepository
+                .findSomeNumbers()
+                .map(x -> x * 2)
+                .collect(Collectors.toList());
+    }
+}
